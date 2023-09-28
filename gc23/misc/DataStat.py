f6aa9021688f61ec2b09ec0061bca6223e54d074
Paul Walther <paul.walther@tum.de>
2023-09-28 13:48:00 +0200

    Improve comments in data stat file

--- gc23/misc/DataStat.py
+++ gc23/misc/DataStat.py
@@ -5,6 +5,8 @@ from torchvision.datasets import ImageFolder
 
 from _config import *
 
+# This script is used to get mean and std of all the training data.
+# To run you need to move imgs to a subfolder
 
 imgMean = [0, 0, 0]
 imgStd  = [0, 0, 0]
@@ -16,8 +18,10 @@ for img, a in dataset:
         imgStd[i]  += img[i, :, :].std()
 print(numpy.array(imgMean)/len(dataset), numpy.array(imgStd)/len(dataset))
 
-# need to move imgs to a subfolder to run this script
 
-# [0.77585715 0.8034677  0.8158493 ]*255 [0.11537416, 0.10468962, 0.10207428]
-# [197.84357325, 204.8842635, 208.0415715]
+
+# Results are:
+# mean: [0.77585715 0.8034677  0.8158493 ] std: [0.11537416, 0.10468962, 0.10207428]
+# mean has to be multiplied by 255 as defined by Detectron: [197.84357325, 204.8842635, 208.0415715]
+# These values are already embedded in the config.
 
